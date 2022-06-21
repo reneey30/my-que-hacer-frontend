d7dc8166006bab423e993b632c0bfc60f3406815
Irene Njeru <irenenjeru8@gmail.com>
2022-06-21 18:30:06 +0300

    styled the Add ToDo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Button, Form } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Alarm, ExclamationCircle } from 'react-bootstrap-icons';
 
 // function AddTodo() {
 //   return (
@@ -9,32 +10,65 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // }
 
 function AddTodo({ todos, setTodos }) {
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-    const addTodo = text => {
-        const newTodos = [...todos, { text }];
-        setTodos(newTodos);
-      };
-  
-    const handleSubmit = e => {
-      e.preventDefault();
-      if (!value) return;
-      addTodo(value);
-      setValue("");
-    };
-  
-    return (
-      <Form className="col-9" onSubmit={handleSubmit}> 
+  const [urgentSelect, setUrgentSelect] = useState(false);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form className="col-9" onSubmit={handleSubmit}>
       <Form.Group>
-        <Form.Label><b>Add Todo </b></Form.Label>
-        <Form.Control type="text" className="input mb-2" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+        <Form.Label>
+          <b>Add Todo </b>
+        </Form.Label>
+        <div className="row">
+          <div className="d-inline-flex">
+            <Form.Control
+              type="text"
+              className="input mb-2"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              placeholder="Add new todo"
+            />
+            <div className="d-flex flex-column justify-content-center ms-2">
+              <Alarm  size={24} className={`ms-1 ${urgentSelect ? "text-danger" : "text-secondary"}`}/>
+              <div className="form-check form-switch mx-auto">
+                <Form.Control
+                  className="input form-check-input"
+                  type="checkbox"
+                  id="flexSwitchCheckDefault"
+                />
+              </div>
+            </div>
+            <div className="d-flex flex-column justify-content-center ms-2">
+              <ExclamationCircle color="grey" size={24} className="ms-1"/>
+              <div className="form-check form-switch mx-auto">
+                <Form.Control
+                  className="input form-check-input"
+                  type="checkbox"
+                  id="flexSwitchCheckDefault"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </Form.Group>
       <Button variant="primary mb-3" type="submit">
         Submit
       </Button>
     </Form>
-    );
-  }
+  );
+}
 
-export default AddTodo
-
+export default AddTodo;
