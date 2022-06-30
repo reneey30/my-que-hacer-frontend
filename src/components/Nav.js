302ef6716c04d7cd195f5a98478a4bac2175ba58
Irene Njeru <irenenjeru8@gmail.com>
2022-06-30 16:40:55 +0300

    add logic to login and register

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,29 +1,35 @@
-import { useState} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Nav({ userId, setUserId, username, setUsername }) {
   const [showRegister, setShowRegister] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
 
-  const logout =  () => {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const logout = () => {
     setUserId = null;
   };
   const register = () => {
     // use fetch to submit form details to our api signup route
-  }
+  };
   const login = () => {
     // use fetch to submit form details to our api signin route
-  }
+  };
 
   const registerHandler = (e) => {
     e.preventDefault();
     register();
-  }
+  };
 
   const loginHandler = (e) => {
     e.preventDefault();
     login();
-  }
+  };
 
   const handleCloseR = () => setShowRegister(false);
   const handleShowR = () => setShowRegister(true);
@@ -33,33 +39,117 @@ function Nav({ userId, setUserId, username, setUsername }) {
 
   return (
     <>
-        <nav className="navbar justify-content-between">
-            <h2>My Que Hacer</h2>
-            <div>
-              <div>
-                {userId ? (
-                  <div className="d-flex justify-content-between">
-                    <div>User: {userId ? username : "Not Logged In"}</div>
+      <nav className="navbar justify-content-between">
+        <h2>My Que Hacer</h2>
+        <div>
+          <div>
+            {userId ? (
+              <div className="d-flex justify-content-between">
+                <div>User: {userId ? username : "Not Logged In"}</div>
 
-                    <button className="btn btn-light mx-2" onClick={logout}>
-                      logout
-                    </button>
-                  </div>
-                ) : (
-                  <div>
-                    <button className="btn btn-light mx-2" onClick={handleShowL}>
-                      Login
-                    </button>
-                    <button className="btn btn-light mx-2" onClick={handleShowR}>
-                      Register
-                    </button>
-                  </div>
-                )}
-              </div>  
-            </div>        
-        </nav>
+                <button className="btn btn-light mx-2" onClick={logout}>
+                  logout
+                </button>
+              </div>
+            ) : (
+              <div>
+                <button className="btn btn-light mx-2" onClick={handleShowL}>
+                  Login
+                </button>
+                <button className="btn btn-light mx-2" onClick={handleShowR}>
+                  Register
+                </button>
+              </div>
+            )}
+          </div>
+
+          {/* Register modal */}
+
+          {/* <Button variant="primary" onClick={handleShowR}>
+              Register
+            </Button> */}
+
+          <Modal show={showRegister} onHide={handleCloseR}>
+            <Modal.Header closeButton>
+              <Modal.Title>Register with Email and Password</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <form onSubmit={registerHandler}>
+                <h3> Register User </h3>
+                <input
+                  type="email"
+                  placeholder="Email..."
+                  className="form-control mb-2"
+                  onChange={(event) => {
+                    setRegisterEmail(event.target.value);
+                  }}
+                />
+                <input
+                  type="password"
+                  placeholder="Password..."
+                  className="form-control mb-2"
+                  onChange={(event) => {
+                    setRegisterPassword(event.target.value);
+                  }}
+                />
+                <button type="submit" className="btn btn-outline-dark mb-2">
+                  Create User
+                </button>
+
+                {/* <button type="submit"> Create User 2</button> */}
+              </form>
+            </Modal.Body>
+          </Modal>
+
+          {/* login modal*/}
+
+          {/* <Button variant="primary" onClick={handleShowL}>
+              Sign In
+            </Button> */}
+
+          <Modal show={showLogin} onHide={handleCloseL}>
+            <Modal.Header closeButton>
+              <Modal.Title>Sign In with Email and Password</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <form onSubmit={loginHandler}>
+                <h3> Login </h3>
+                <input
+                  type="email"
+                  placeholder="Email..."
+                  className="form-control mb-2"
+                  onChange={(event) => {
+                    setLoginEmail(event.target.value);
+                  }}
+                />
+                <input
+                  type="password"
+                  placeholder="Password..."
+                  className="form-control mb-2"
+                  onChange={(event) => {
+                    setLoginPassword(event.target.value);
+                  }}
+                />
+                <button type="submit" className="btn btn-outline-dark mb-2">
+                  Sign In
+                </button>
+
+                {/* <button onClick={login}> Login</button> */}
+              </form>
+            </Modal.Body>
+            {/* <Modal.Footer>
+                <Button variant="secondary" onClick={handleCloseL}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={login}>
+                  Log In
+                </Button>
+              </Modal.Footer> */}
+          </Modal>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
